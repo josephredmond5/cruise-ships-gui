5391c8bd0758ba4fdc95f2458fd42d86b664cac0
josephredmond5 <josephredmond5@icloud.com>
2023-01-16 20:18:39 +0000

    modified itinerary test suite

--- src/Itinerary.js
+++ src/Itinerary.js
@@ -1,3 +1,5 @@
+const { blue } = require("color-name");
+
 function Itinerary(ports) {
   this.ports = ports;
 };
@@ -6,22 +8,5 @@ function Itinerary(ports) {
 
 module.exports = Itinerary;
 
-* the this keyword refers to the object that it is cotained inside of
- || that it is used inside of 
- || that it is coded inside of *
 
-const person = {
-  name: 'joseph';
-  age: 25;
-  console.log(person);
-}
-
-personGrowsUp() {
-  this.age++;
-}
-
-personGrowsUp();
-
-// ask ryan to explain this when at his tomorrow 
-// ask ryan about methods and functions and objects
 
