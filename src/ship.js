7cbbee6dc9297abb7cffef4bf7357713f2e2b385
josephredmond5 <josephredmond5@icloud.com>
2023-01-16 21:01:49 +0000

    need to look over this again. but, this is the finished version of ships have itinerarys that have ports

--- src/ship.js
+++ src/ship.js
@@ -1,16 +1,28 @@
-function Ship (currentPort) {
-  this.currentPort = currentPort;
-  this.previousPort = null;
+function Ship (itinerary) {
+  this.itinerary = itinerary;
+  this.currentPort = itinerary.ports[0];
+  this.previousPort = null
 }
 
 Ship.prototype = {
   setSail() {
+    const itinerary = this.itinerary;
+    const currentPortIndex = itinerary.ports.indexOf(this.currentPort);
+  
+    if (currentPortIndex === (itinerary.ports.length - 1)) {
+      throw new Error('End of itinerary reached');
+    }
+  
     this.previousPort = this.currentPort;
     this.currentPort = null;
   },
-    dock(port) {
-      this.currentPort = port;
-    },
+    
+  dock () {
+    const itinerary = this.itinerary;
+    const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
+  
+    this.currentPort = itinerary.ports[previousPortIndex + 1];
+  }
   };
 
 
